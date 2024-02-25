14dda5c6454ff9673bda1775c2d4dac3adffb33d
Entersoft4 <alperen7434@gmail.com>
2024-02-25 15:37:13 +0300

    UpdateV2.0

--- OTOMASYON_SISTEMI/OTOMASYON_SISTEMI/obj/Debug/net8.0-windows/OTOMASYON_SISTEMI.AssemblyInfo.cs
+++ OTOMASYON_SISTEMI/OTOMASYON_SISTEMI/obj/Debug/net8.0-windows/OTOMASYON_SISTEMI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OTOMASYON_SISTEMI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d9a4de801a43010ce28c644151f04c45c816a8fd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fcd6e7e8b88ef50e5706a7990ebbb781eaa265da")]
 [assembly: System.Reflection.AssemblyProductAttribute("OTOMASYON_SISTEMI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OTOMASYON_SISTEMI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
